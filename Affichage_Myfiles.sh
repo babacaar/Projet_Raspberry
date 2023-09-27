57efe6ccb6caa947942c088796cf6d4cdf0d813e
root <techinfo@lpjw.fr>
2023-09-27 15:39:38 +0200

    Commit après modif bdd todolist pour vue tache

--- Affichage_Myfiles.sh
+++ Affichage_Myfiles.sh
@@ -12,7 +12,7 @@
 
 			#/usr/bin/chromium-browser --kiosk --noerrdialogs http://10.49.11.214/captures/capturemm.png http://10.49.11.214/captures/capturemm.png http://10.49.11.214/captures/capturemm.png https://lpjw.fr/ecrans/menu.jpg &
 
-			/usr/bin/chromium-browser --kiosk --noerrdialogs http://192.168.250.1/ https://lpjw.fr/ecrans/menu.jpg &
+			/usr/bin/chromium-browser --kiosk --noerrdialogs http://192.168.250.1/ https://lpjw.fr/ecrans/menu.jpg http://172.17.5.202/Vue_Taches.php &
 
 
 		while true; do
