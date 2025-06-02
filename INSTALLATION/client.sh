8136dab599b6e63f3a0914b618aafd2bc43c317a
root <techinfo@lpjw.fr>
2025-06-02 16:16:32 +0200

    Correction script client

--- INSTALLATION/client.sh
+++ INSTALLATION/client.sh
@@ -40,7 +40,7 @@ for CHOICE in $CHOICES; do
         1)
             show_info "Installation des paquets" "Mise à jour et installation des paquets nécessaires..."
             run_command "sudo apt update" "Mise à jour des paquets"
-            run_command "sudo apt install -y xdotool unclutter libnss3-tools cec-utils wmctrl mpv proftpd" "Installation des outils système"
+            run_command "sudo apt install -y  cec-utils xdotool unclutter libnss3-tools cec-utils wmctrl mpv proftpd" "Installation des outils système"
             ;;
         2)
             show_info "Configuration LightDM" "Ajout de l'utilisateur 'pi' pour autologin"
@@ -78,15 +78,236 @@ Environment=XAUTHORITY=/home/pi/.Xauthority
 [Install]
 WantedBy=graphical.target
 EOF
-            # Répéter pour alertPpms.service et kiosk.service...
+
+#Service Alerte PPMS
+cat <<EOF | sudo tee /lib/systemd/system/alertPpms.service > /dev/null
+[Unit]
+Description=Alertest Service
+Wants=graphical.target
+After=graphical.target
+
+[Service]
+Type=simple
+ExecStart=/bin/bash /home/pi/alertPpms.sh
+Restart=on-abort
+User=pi
+Group=pi
+Environment=DISPLAY=:0.0
+Environment=XAUTHORITY=/home/pi/.Xauthority
+
+[Install]
+WantedBy=graphical.target
+EOF
+
+#Service Kiosque Chromium
+cat <<EOF | sudo tee /lib/systemd/system/kiosk.service > /dev/null
+[Unit]
+Description=Chromium Kiosk
+Wants=graphical.target
+After=graphical.target
+
+[Service]
+Environment=DISPLAY=:0.0
+Environment=XAUTHORITY=/home/pi/.Xauthority
+Type=simple
+ExecStart=/bin/bash /home/pi/kiosk.sh 
+Restart=on-abort
+User=pi
+Group=pi
+
+[Install]
+WantedBy=graphical.target
+EOF
             ;;
         5)
             show_info "Création des scripts .sh" "Les scripts seront créés dans /home/pi"
             # Répéter le contenu des scripts avec `cat <<EOF` comme dans votre script initial
-            # Par exemple :
             cat <<EOF > /home/pi/reboot.sh
 #!/bin/bash
 sudo reboot
+EOF
+
+cat <<EOF | tee /home/pi/alert_feu.sh > /dev/null
+#!/bin/bash
+
+sudo systemctl start alertFeu.service
+EOF
+
+#Script Alertfeu pour lancer l'alerte incendie
+cat <<EOF | tee /home/pi/alertFeu.sh > /dev/null
+#!/bin/bash
+
+sudo systemctl stop kiosk.service
+sudo systemctl stop alertPpms.service
+
+xset s noblank\n
+xset s off\n
+xset -dpms\n
+unclutter -idle 1 -root &\n
+/usr/bin/chromium-browser --kiosk --noerrdialogs http://ip/bar_info.php
+EOF
+
+
+#Script Alerte_ppms pour lancer le service
+cat <<EOF | tee /home/pi/alert_ppms.sh > /dev/null
+#!/bin/bash
+
+sudo systemctl start alertPpms.service
+
+EOF
+
+
+#Script AlertPpms pour lancer l'alerte PPMS
+cat <<EOF | tee /home/pi/alertPpms.sh > /dev/null
+#!/bin/bash
+
+sudo systemctl stop kiosk.service
+sudo systemctl stop alertFeu.service
+xset s noblank\n
+xset s off\n
+xset -dpms\n
+unclutter -idle 1 -root &\n
+/usr/bin/chromium-browser --kiosk --noerrdialogs http://ip/bar_ppms.php
+EOF
+
+
+
+#Script test.sh pour lancer le kiosque
+cat <<EOF | tee /home/pi/test.sh > /dev/null
+#!/bin/bash
+sudo systemctl stop alertPpms.service
+sudo systemctl stop alertFeu.service
+date01=$(date +"%F_%T")
+sudo systemctl stop kiosk.service
+sudo mkdir -p /home/pi/KioskOld/$date01
+sudo cp /home/pi/kiosk.sh /home/pi/KioskOld/$date01/
+sudo sleep 15
+sudo cp -f /home/pi/Myfiles /home/pi/kiosk.sh
+sudo chmod 744 /home/pi/kiosk.sh
+sudo chown root:root /home/pi/kiosk.sh
+sudo dos2unix /home/pi/kiosk.sh
+sudo systemctl start kiosk.service
+EOF
+
+
+#Script kiosk.sh pour lancer l'affichage des liens
+cat <<EOF | tee /home/pi/kiosk.sh > /dev/null
+!/bin/bash
+#Compteur d'itérations
+compteur=0;
+#Fonction pour lancer Chromium
+lancer_chromium() {
+    xset s noblank
+    xset s off
+    xset -dpms
+    unclutter -idle 1 -root &
+ /usr/bin/chromium-browser --kiosk --noerrdialogs https://affichage.lpjw.local/display_absences.php https://affichage.lpj.local/menu.jpg
+}
+
+fermer_onglets_chromium() {
+    xdotool search --onlyvisible --class "chromium-browser" windowfocus key ctrl+shift+w
+    wmctrl -k off
+}
+
+#Fonction pour arrêter Chromium
+arreter_chromium() {
+    killall chromium-browser
+    #Ajoutez ici dutres commandes pour nettoyer l'environnement si nécessaire
+}
+
+#Lancer Chromium au début
+lancer_chromium
+
+while true; do
+    xdotool keydown ctrl+Next
+    xdotool keyup ctrl+Next
+
+    xdotool keydown ctrl+r
+    xdotool keyup ctrl+r
+
+    sleep 15
+
+    #Incrémente le compteur d'itérations
+    ((compteur++))
+
+    #Vérifie si le nombre d'itérations spécifié est atteint
+    if [ "$compteur" -eq "3" ]; then
+        #Arrêtez le processus Chromium
+        #arreter_chromium
+        #fermer_onglets_chromium
+        #Lancement de la vidéo avec VLC
+        mpv --fs /home/pi/Videos/video.mp4
+        #sleep 10
+        #Attendez que VLC se termine avant de réinitialiser le compteur
+        #Relancer Chromium après que VLC ait terminé
+        lancer_chromium
+
+        #Réinitialisez le compteur
+        compteur=0
+    fi
+done
+EOF
+
+#Script pour lancer l'affichage de l'information ponctuelle
+cat <<EOF | tee /home/pi/time.sh > /dev/null
+
+#!/bin/bash
+duree=$1
+echo "Valeur de duree: $duree"
+
+        if ! [[ "$duree" =~ ^[0-9]+$ ]]; then
+                echo "La valeur de duree n'est pas un nombre entier positif."
+                exit 1
+        fi
+
+sudo systemctl stop kiosk.service
+sudo cp -f /home/pi/MyfilesInfo /home/pi/kiosk.sh
+sudo chmod 744 /home/pi/kiosk.sh
+sudo chown root:root /home/pi/kiosk.sh
+sudo dos2unix /home/pi/kiosk.sh
+sudo systemctl start kiosk.service
+
+# Attendre que la durée soit écoulée
+while [ "$duree" -gt 0 ]; do
+    sleep 1
+    ((duree--))
+done
+
+sudo cp -f /home/pi/Myfiles /home/pi/kiosk.sh
+sudo dos2unix /home/pi/kiosk.sh
+sudo systemctl restart kiosk.service
+EOF
+
+#Script reboot pour lancer le redémarrage du périphérique
+cat <<EOF | tee /home/pi/tv_off.sh > /dev/null
+
+echo 'standby 0.0.0.0' | cec-client -s -d 1
+
+EOF
+
+cat <<EOF | tee /home/pi/tv_on.sh > /dev/null
+
+echo 'on 0.0.0.0' | cec-client -s -d 1
+
+EOF
+
+cat <<EOF | tee /home/pi/tv_statut.sh > /dev/null
+
+echo 'pow 0.0.0.0' | cec-client -s -d 1
+
+EOF
+
+
+
+
+cat <<EOF | tee /home/pi/closeService.sh > /dev/null
+
+# Fermer tous les onglets de Chromium ou Firefox
+pkill chromium
+pkill firefox
+
+# Arrêter les services
+sudo systemctl stop kiosk.service
 EOF
             chmod +x /home/pi/*.sh
             ;;
