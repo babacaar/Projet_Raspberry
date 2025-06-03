50cda8bbf1048b1f73ef4bf0f5b225060e062f12
root <techinfo@lpjw.fr>
2025-06-03 18:01:47 +0200

    modifi

--- controllers/controller_delgroupe.php
+++ controllers/controller_delgroupe.php
@@ -20,31 +20,23 @@ include "../modules/header.php";
                 if ($_SERVER['REQUEST_METHOD'] === 'POST' && isset($_POST['groupIDs']) && is_array($_POST['groupIDs'])) {
                     // Établir la connexion à la base de données
                     $pdo = new PDO('mysql:host=' . $dbhost . ';port=' . $dbport . ';dbname=' . $db, $dbuser, $dbpasswd);
+                    $pdo->setAttribute(PDO::ATTR_ERRMODE, PDO::ERRMODE_EXCEPTION);
 
-                    // Supprimer les groupes sélectionnés de la base de données
+                    // Récupérer les IDs à supprimer
                     $groupIDs = $_POST['groupIDs'];
                     $placeholders = implode(',', array_fill(0, count($groupIDs), '?'));
 
-                    $sql = "DELETE FROM groupes WHERE id IN ($placeholders)";
-                    $stmt = $pdo->prepare($sql);
-                    $stmt->execute($groupIDs);
+                    // Supprimer d'abord les dépendances dans pis_groups
+                    $deleteDependenciesSQL = "DELETE FROM pis_groups WHERE group_id IN ($placeholders)";
+                    $deleteDependenciesStmt = $pdo->prepare($deleteDependenciesSQL);
+                    $deleteDependenciesStmt->execute($groupIDs);
 
-                    // Récupérer les groupes restants après la suppression
-                    $query = "SELECT id, name, membres FROM groupes";
-                    $stmt = $pdo->query($query);
-                    $remainingGroups = $stmt->fetchAll(PDO::FETCH_ASSOC);
+                    // Supprimer les groupes sélectionnés
+                    $deleteGroupsSQL = "DELETE FROM groupes WHERE id IN ($placeholders)";
+                    $deleteGroupsStmt = $pdo->prepare($deleteGroupsSQL);
+                    $deleteGroupsStmt->execute($groupIDs);
 
-                    // Réorganiser les ID de manière continue
-                    $newID = 1;
-                    foreach ($remainingGroups as $group) {
-                        // Mettre à jour l'ID du groupe dans la base de données
-                        $updateQuery = "UPDATE groupes SET id = ? WHERE id = ?";
-                        $updateStmt = $pdo->prepare($updateQuery);
-                        $updateStmt->execute([$newID, $group['id']]);
-                        $newID++;
-                    }
-
-                    $msg = "Le groupe a bien été supprimé.";
+                    $msg = "Les groupes sélectionnés ont bien été supprimés.";
                     include "../modules/success.php";
                 }
             } catch (PDOException $e) {
