8deffea9374810acd2fb0fb57452239a839658e8
root <techinfo@lpjw.fr>
2025-06-05 10:35:20 +0200

    RELEASE DU 05/06/2025

--- certificates.php
+++ certificates.php
@@ -1,6 +1,6 @@
 <!------------HEADER------------>
 <?php
-require_once "/var/www/monsite.fr/verif_session.php";
+require_once "verif_session.php";
 $pageTitle = "Chargement autorité de certification"; // Titre de la page
 $dropDownMenu = true;
 include "modules/header.php";
